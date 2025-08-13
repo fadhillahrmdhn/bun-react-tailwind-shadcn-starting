0ad428de10db3b12d6c521438a43e24722e089a6
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-08-13 18:57:11 +0700

    refactor: define SearchBarProps interface for better type clarity

--- src/components/shared/SearchBar.tsx
+++ src/components/shared/SearchBar.tsx
@@ -1,10 +1,11 @@
 import { Input } from '@/components/ui/input';
-import { useEffect, useMemo, useState } from 'react';
-import type { Character } from '@/types';
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '../ui/card';
-import { DragonBallCharacterCard } from './CharacterCard';
-export const SearchBar = ( { search, setSearch }: { search: string; setSearch: (value: string) => void }) => {
 
+interface SearchBarProps {
+  search: string;
+  setSearch: (value: string) => void;
+}
+
+export const SearchBar = ({ search, setSearch }: SearchBarProps) => {
   return (
     <div className="flex flex-col gap-10">
       <Input className='bg-white dark:bg-gray-800 w-full lg:w-3/4 self-end' type="text" placeholder="Search characters..." value={search} onChange={(e) => setSearch(e.target.value)} />
