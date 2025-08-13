d040644609e15509e30ebfa6abac62dfbead4c02
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-08-13 23:53:27 +0700

    style: add responsive width to DataTable component for improved layout

--- src/components/shared/dragonball/data-table.tsx
+++ src/components/shared/dragonball/data-table.tsx
@@ -29,7 +29,7 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="overflow-hidden rounded-md border bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
+    <div className=" overflow-hidden rounded-md border bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700 md:w-2xl">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
