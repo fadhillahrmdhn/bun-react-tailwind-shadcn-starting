0dc064042c252dae267059245815ccb0f73aa9b9
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-08-14 10:21:18 +0700

    feat: add pagination to DragonBall component

--- src/components/shared/dragonball/FetchDragonBall.tsx
+++ src/components/shared/dragonball/FetchDragonBall.tsx
@@ -1,11 +1,12 @@
 import { useMemo, useState, useCallback } from 'react';
 import type { DragonballItem, DragonballResponse } from '@/interfaces';
 import { SearchBar, ButtonTheme } from '@/components/shared';
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/lib/api';
 import { columns } from './columns';
 import { DataTable } from './data-table';
 import { useQuery } from '@tanstack/react-query';
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
 
 import type { Limit } from '@/types';
 
@@ -42,7 +43,7 @@ export const DragonBall = () => {
     } catch (error) {
       console.error('Error fetching Dragonball data:', error);
       return fallback;
-    } 
+    }
   }, [page, limit]);
 
   const { isPending, isError, data, error, isLoading } = useQuery({
@@ -54,11 +55,26 @@ export const DragonBall = () => {
     return (data?.items ?? []).filter((char: DragonballItem) => char.name.toLowerCase().includes(search.toLowerCase())).sort((a, b) => a.id - b.id);
   }, [search, data]);
 
+  const totalPages = data?.meta?.totalPages ?? 1;
+  const currentPage = data?.meta?.currentPage ?? 1;
+
+  const handlePreviousPage = (): void => {
+    setPage((page) => Math.max(page - 1, 1));
+  };
+
+  const handleNextPage = (): void => {
+    setPage((page) => Math.min(page + 1, totalPages));
+  };
+
+  const handlePageClick = (pageNumber: number): void => {
+    setPage(pageNumber);
+  };
+
   return (
     <div className="grid grid-cols-1 gap-10">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-0">
         <ButtonTheme />
-        <SearchBar search={search} setSearch={setSearch} />
+        {/* <SearchBar search={search} setSearch={setSearch} /> */}
       </div>
 
       {isPending && !data ? (
@@ -67,14 +83,35 @@ export const DragonBall = () => {
           <Skeleton className="h-64 w-full" />
         </div>
       ) : filteredCharacters.length > 0 ? (
-        <div className=''>
-        <DataTable columns={columns} data={filteredCharacters} />          
+        <div className="">
+          <DataTable columns={columns} data={data?.items ?? []} />
         </div>
       ) : (
         <div className="text-center p-4">
           <p>No characters found.</p>
         </div>
       )}
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem className={currentPage === 1 ? 'pointer-events-none opacity-40' : 'cursor-pointer'}>
+            <PaginationPrevious onClick={handlePreviousPage} />
+          </PaginationItem>
+
+          {Array.from({ length: totalPages }, (_, i) => (
+            <PaginationItem key={i} className='cursor-pointer'>
+              <PaginationLink onClick={() => handlePageClick(i + 1)}>{i + 1}</PaginationLink>
+            </PaginationItem>
+          ))}
+
+          {/* <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem> */}
+
+          <PaginationItem className={currentPage === totalPages ? 'pointer-events-none opacity-40' : 'cursor-pointer'}>
+            <PaginationNext  onClick={handleNextPage} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
