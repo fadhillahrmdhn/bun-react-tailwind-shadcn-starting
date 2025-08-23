4783271f2301448df48f6553dded44216da204c5
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-08-23 11:05:57 +0700

    fix: update api2 baseURL to use the correct authentication URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,7 +10,7 @@ export const api: AxiosInstance = axios.create({
 });
 
 export const api2: AxiosInstance = axios.create({
-  baseURL: process.env.BUN_PUBLIC_DRAGONBALL_URL as string,
+  baseURL: process.env.BUN_PUBLIC_AUTH_URL as string,
   timeout: 10000,
 });
 
